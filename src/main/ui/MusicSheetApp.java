e80e6282056a1ce5def7118c3dd94b93c4a408cb
tiang01 <tiang01@students.cs.ubc.ca>
2021-02-14 15:34:12 -0800

    Did a little bit of fine-tuning

--- src/main/ui/MusicSheetApp.java
+++ src/main/ui/MusicSheetApp.java
@@ -3,13 +3,11 @@ package ui;
 import model.Notes;
 import model.SheetMusic;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MusicSheetApp {
     private Notes note;
     private SheetMusic score;
-    private ArrayList<Notes> sheet;
     private Scanner input; //Teller App
 
     public MusicSheetApp() {
@@ -47,7 +45,7 @@ public class MusicSheetApp {
     // EFFECTS: displays menu of options to user
     // Note: code inspired by Teller App
     private void displayMenu() {
-        System.out.println("\nSelect from:");
+        System.out.println("\nWhat would you like to do?");
         System.out.println("\ta -> Add Note");
         System.out.println("\tr -> Remove Note");
         System.out.println("\tc -> Clear all the Notes");
@@ -78,8 +76,8 @@ public class MusicSheetApp {
     // MODIFIES: this
     // EFFECTS: adds a new note
     private void doAddNote() {
-        System.out.print("Enter the letter of the note that you would like to add:");
-        System.out.print(" (Select from A, B, C, D, E, F, or G)");
+        System.out.print("Enter the letter of the note that you would like to add: ");
+        System.out.print("\n(Select from A, B, C, D, E, F, or G)");
         String noteName = input.next();
         note = new Notes(noteName);
         if (noteName.equalsIgnoreCase("A")
@@ -99,7 +97,7 @@ public class MusicSheetApp {
     // EFFECTS: removes a note
     private void doRemoveNote() {
         System.out.print("Enter the letter of the note that you would like to remove:");
-        System.out.print(" (Select from A, B, C, D, E, F, or G)");
+        System.out.print("\n(Select from A, B, C, D, E, F, or G)");
         String noteName = input.next();
         note = new Notes(noteName);
         if (noteName.equalsIgnoreCase("A")
